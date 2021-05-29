bd77d8f20164ed17001bf384dd7f65148e4896f4
Spartak <assorium@gmail.com>
2021-05-29 15:07:12 +0300

    removed ackee

--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,50 +1,42 @@
 <template>
-	<div class="container mx-auto flex p-4 bg-white rounded-lg shadow-xl dark:bg-black dark:bg-opacity-50 dark:shadow-xl-dark">
-		<div class="flex-1">
-			<img class="h-32 w-32 rounded-lg" src="/img/avatar2.jpg" />
+	<div class="wrapper">
+		<div class="sidebar">
+			<ul class="pt-2">
+				<li class="pb-2">Обо мне</li>
+				<li class="pb-2">Проекты</li>
+				<li class="pb-2">Фотографии</li>
+				<li class="pb-2">Статьи</li>
+				<li class="pb-2">Образование</li>
+				<li class="pb-2">Опыт работы</li>
+			</ul>
 		</div>
-		<div class="ml-4 flex-2">
-			<h1 class="text-xl tracking-wide leading-tight mb-1">{{ $t('my.name') }}</h1>
-			<h2 class="text-base leading-relaxed mb-2 text-black dark:text-white text-opacity-50 dark:text-opacity-50">{{ $t('my.description') }}</h2>
-
-			<div class="flex items-start text-3xl mt-3">
-				<div class="social">
-					<a href="https://github.com/mrspartak" target="_blank" rel="noopener noreferer" class="">
-						<i class="bx bxl-github"></i>
-					</a>
-				</div>
-				<div class="social">
-					<a href="https://www.instagram.com/assorium/" target="_blank" rel="noopener noreferer">
-						<i class="bx bxl-instagram"></i>
-					</a>
-				</div>
-				<div class="social">
-					<a href="https://www.linkedin.com/in/spartak-kagramanyan-87172361/" target="_blank" rel="noopener noreferer">
-						<i class="bx bxl-linkedin"></i>
-					</a>
-				</div>
-			</div>
+		<div class="content">
+			<about class="mb-3 card" />
 		</div>
 	</div>
 </template>
 
 <script>
+import About from './blocks/About.vue';
+
 export default {
 	name: 'Landing',
+	components: {
+		About,
+	},
 };
 </script>
 
 <style>
-.container {
+.sidebar {
+	width: 150px;
+	position: fixed;
+	margin-left: -160px;
+}
+.card {
 	position: relative;
 	width: auto;
 	z-index: 2;
 	transition: all 0.25s ease;
-}
-.social {
-	@apply flex-initial text-center mr-3 opacity-75 transition-all duration-500 ease-out;
-}
-.social:hover {
-	@apply opacity-100;
 }
 </style>
